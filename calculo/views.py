a35d4760791c637a79d94a48a6c885321b04e2f6
Matheus Muriel <matheus.muriel@outlook.com>
2019-08-28 13:26:34 -0300

    Sliter de dados da requisição

--- calculo/views.py
+++ calculo/views.py
@@ -4,10 +4,16 @@ from .serializer import CalculoSerializer
 from rest_framework.decorators import api_view
 from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import re
 
 @csrf_exempt
 def calcular(request):
     if request.method == 'POST':
-        #funcao = str(request.POST['funcao'])
-        print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        dados = str(request.body)
+        valores = spliter_request(dados)
+
         return HttpResponse("a")
+
+def spliter_request(dados):
+	dados_limpos = re.sub(r"(\[|\]|\\|\'|b|\")","",dados)
+	return dados_limpos.split(",")
