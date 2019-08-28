804700c1576bbdd18e7d002a6871acdd085de5df
Matheus Muriel <matheus.muriel@outlook.com>
2019-08-28 14:49:38 -0300

    Passando a requisição para o processador de calculo

--- calculo/calculadora.py
+++ calculo/calculadora.py
@@ -1,74 +1,91 @@
 
-#from numpy import array
-
-x5, x4, x3, x2, x1, x = 0.000, 0.000, 1.000, 0.000, -9.000, 3.000
-calc_intervalo_real_num = []
-interacaoX= []
-interacaoFdeX= []
-interacaoBmenosA= []
-
-def calcFdeX(interacao):
-    return x5*interacao**5 + x4*interacao**4 + x3*interacao**3 + x2*interacao**2 + x1*interacao + x
-
-def calcularIsolamento(intervalo):
-    resultadoFinal =[]
-    for _ in range (intervalo*-2):
-        result = calcFdeX(intervalo)
-        intervalo = intervalo +1
-        resultadoFinal.append(result) 
-    return resultadoFinal
-
-def calcularIntervalo(array_resultado, intervalo):
-    resultadoFinal =[]
-    for i in range (intervalo*-2-1):
-        if (array_resultado[i]<0 and array_resultado[i+1]>0) or (array_resultado[i]>0 and array_resultado[i+1]<0) :
-            resultadoFinal.append(intervalo)
-            resultadoFinal.append(intervalo+1)
-            calc_intervalo_real_num.append(array_resultado[i])
-            calc_intervalo_real_num.append(array_resultado[i+1])
-        intervalo = intervalo+1
-    return resultadoFinal
-
-def calcZeroReais (a, b):
-    resultFdeX = 0
-    interacao = (a+b)/2
-    criterio_de_parada = abs((b-a)/2)
-    resultFdeX = calcFdeX(interacao)
-    if criterio_de_parada < 10**-3:
-        interacaoX.append(interacao)
-        return interacaoX
-    print(interacao, resultFdeX, criterio_de_parada)
-#     print(interacao)
-#     print(resultFdeX)
-    interacaoX.append(interacao) #esse e o resultado do b - a
-    interacaoFdeX.append(resultFdeX)
-    interacaoBmenosA.append(abs(b-a)/2)
-
-    if resultFdeX < 0:
-        calcZeroReais(a, interacao)
-    if resultFdeX > 0:
-        calcZeroReais(interacao, b)
-def filtraParaCalcular(calc_intervalo_real_num, calc_intervalo):
-    #calcZeroReais(0, 1)
-    tamanho_array = len(calc_intervalo)
-    for i in range (0, tamanho_array, 2):
-        if calc_intervalo_real_num[i]>0:
-            print(calc_intervalo[i], calc_intervalo[i+1])
-            calcZeroReais(calc_intervalo[i], calc_intervalo[i+1])
-        else:
-            print(calc_intervalo[i], calc_intervalo[i+1])
-            calcZeroReais(calc_intervalo[i+1], calc_intervalo[i])
-
-intervalo = -5
-resultado = calcularIsolamento(intervalo)
-calc_intervalo = calcularIntervalo(resultado, intervalo)
-filtraParaCalcular(calc_intervalo_real_num, calc_intervalo)
-# print(interacaoX)
-# print(interacaoFdeX)
-# print(interacaoBmenosA)
-# print(resultado)
-# print(calc_intervalo_real_num)
-# print(calc_intervalo)
-#print(calcFdeX(x5, x4, x3, x2, x1, x, -5))
+class calculadoraFuncao():    
+    def __init__(self, _x5, _x4, _x3, _x2, _x1, _x):
+        self.x5 = float(_x5)
+        self.x4 = float(_x4)
+        self.x3 = float(_x3)
+        self.x2 = float(_x2)
+        self.x1 = float(_x1)
+        self.x  = float(_x)
+        
+        self.calc_intervalo_real_num = []
+        self.interacaoX = []
+        self.interacaoFdeX = []
+        self.interacaoBmenosA = []
 
 
+    def calcFdeX(self, interacao):
+        return self.x5*interacao**5 + self.x4*interacao**4 + self.x3*interacao**3 + self.x2*interacao**2 + self.x1*interacao + self.x
+
+    def calcularIsolamento(self, intervalo):
+        resultadoFinal = []
+        
+        for _ in range (intervalo*-2):
+            result = self.calcFdeX(intervalo)
+            intervalo = intervalo + 1
+            resultadoFinal.append(result) 
+
+        return resultadoFinal
+
+    def calcularIntervalo(self, array_resultado, intervalo):
+        resultadoFinal = []
+
+        for i in range (intervalo*-2-1):
+            if (array_resultado[i]<0 and array_resultado[i+1]>0) or (array_resultado[i]>0 and array_resultado[i+1]<0) :
+                resultadoFinal.append(intervalo)
+                resultadoFinal.append(intervalo+1)
+                self.calc_intervalo_real_num.append(array_resultado[i])
+                self.calc_intervalo_real_num.append(array_resultado[i+1])
+            intervalo = intervalo + 1
+
+        return resultadoFinal
+
+    def calcZeroReais (self, a, b):
+        resultFdeX = 0
+        interacao = (a+b)/2
+        criterio_de_parada = abs((b-a)/2)
+        resultFdeX = self.calcFdeX(interacao)
+
+        if criterio_de_parada < 10**-3:
+            self.interacaoX.append(interacao)
+            return self.interacaoX
+
+        print(interacao, resultFdeX, criterio_de_parada)
+        #print(interacao)
+        #print(resultFdeX)
+        self.interacaoX.append(interacao) #esse e o resultado do b - a
+        self.interacaoFdeX.append(resultFdeX)
+        self.interacaoBmenosA.append(abs(b-a)/2)
+
+        if resultFdeX < 0:
+            self.calcZeroReais(a, interacao)
+        if resultFdeX > 0:
+            self.calcZeroReais(interacao, b)
+
+    def filtraParaCalcular(self, calc_intervalo_real_num, calc_intervalo):
+        #calcZeroReais(0, 1)
+        tamanho_array = len(calc_intervalo)
+
+        for i in range (0, tamanho_array, 2):
+            if calc_intervalo_real_num[i]>0:
+                print(calc_intervalo[i], calc_intervalo[i+1])
+                self.calcZeroReais(calc_intervalo[i], calc_intervalo[i+1])
+            else:
+                print(calc_intervalo[i], calc_intervalo[i+1])
+                self.calcZeroReais(calc_intervalo[i+1], calc_intervalo[i])
+
+    def calcular(self):
+        intervalo = -5
+        resultado = self.calcularIsolamento(intervalo)
+        calc_intervalo = self.calcularIntervalo(resultado, intervalo)
+        self.filtraParaCalcular(self.calc_intervalo_real_num, calc_intervalo)
+        # print(interacaoX)
+        # print(interacaoFdeX)
+        # print(interacaoBmenosA)
+        # print(resultado)
+        # print(calc_intervalo_real_num)
+        # print(calc_intervalo)
+        #print(calcFdeX(x5, x4, x3, x2, x1, x, -5))
+
+def factor(x5, x4, x3, x2, x1, x):
+    return calculadoraFuncao(x5, x4, x3, x2, x1, x)
--- calculo/views.py
+++ calculo/views.py
@@ -5,15 +5,21 @@ from rest_framework.decorators import api_view
 from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import re
+from .calculadora import factor
 
 @csrf_exempt
 def calcular(request):
-    if request.method == 'POST':
-        dados = str(request.body)
-        valores = spliter_request(dados)
-
-        return HttpResponse("a")
+	if request.method == 'POST':
+		dados = str(request.body)
+		v = spliter_request(dados)
+		c = executarCalculo(v[0], v[1], v[2], v[3], v[4], v[5])
+		return HttpResponse("a")
+	pass
 
 def spliter_request(dados):
 	dados_limpos = re.sub(r"(\[|\]|\\|\'|b|\")","",dados)
 	return dados_limpos.split(",")
+
+def executarCalculo(x5, x4, x3, x2, x1, x):
+	calc = factor(x5, x4, x3, x2, x1, x)
+	calc.calcular()
