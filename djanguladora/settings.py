e430bb6b51083fd266ecc817f51854ffd0a1295c
Matheus Muriel <matheus.muriel@outlook.com>
2019-09-06 19:47:32 -0300

    Remapeado arquivos estaticos

--- djanguladora/settings.py
+++ djanguladora/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'djanguladora.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
+        'DIRS': [os.path.join(BASE_DIR, 'public')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
