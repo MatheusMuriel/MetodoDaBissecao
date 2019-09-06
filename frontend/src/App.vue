02408388b426fbc8c36df3830785a44fdff76d34
Matheus Muriel <matheus.muriel@outlook.com>
2019-09-06 19:03:00 -0300

    Correções no plot

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -97,7 +97,7 @@ export default {
       let v = this.valores
       let l = ['x^5 + ', 'x^4 + ', 'x^3 + ', 'x^2 + ', 'x^1 + ', '']
       let newArr = []
-      for (let i = 0; i < v.length; i++) {
+      for (let i = 0; i < v.length -1; i++) {
         newArr.push(v[i])
         newArr.push(l[i])
       }
