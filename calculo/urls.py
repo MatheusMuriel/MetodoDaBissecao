985db41b1b8d3d3a61560a0fb6df797936f0bc74
Muriel <matheus.muriel@outlook.com>
2019-10-21 14:59:13 -0300

    Separado do projeto do back

--- calculo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .views import *
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    url(r'^calculo/$', calcular, name='calcular'),
-    url(r'^gauss/$', gauss, name='gauss'),
-    url('', index_view),
-]
